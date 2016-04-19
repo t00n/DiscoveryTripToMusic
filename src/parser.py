e38c0aa20f9cacba947d975675492cd03f012175
t00n <kelkununtel@hotmail.com>
2016-04-19 23:45:11 +0200

    [add] key signature feature

--- src/parser.py
+++ src/parser.py
@@ -22,7 +22,8 @@ def create_song_features(data):
             * phrases based features
         "proportion of strong notes" was replaced by "note with highest velocity"
     """
-    time_signature = map(int, list(filter(lambda x: x[2] == "Time_signature", data))[0][3:])
+    time_signature = map(int, list(filter(lambda x: x[2] == "Time_signature", data))[0][-4:])
+    key_signature = list(filter(lambda x: x[2] == "Key_signature", data))[0][-2:]
     notes = DataFrame(list(map(lambda x: [int(x[0]), int(x[1]), x[2], int(x[3]), int(x[4]), int(x[5])], filter(lambda x: x[2][:4] == "Note", data))))
     tempo = int(list(filter(lambda x: x[2] == "Tempo", data))[0][3])
     total_time = notes[1].max()
@@ -76,7 +77,7 @@ def create_song_features(data):
     notes_on = notes_on()
     unique, density = np.unique(DBSCAN(400).fit_predict(notes_on.values.reshape(-1, 1)), return_counts=True)
 
-    return [bpm, pitch().max(), pitch().min(), pitch().mean(), pitch().std(), proportion_high(), proportion_medium(), proportion_bass(), duration.max(), duration.min(), duration.mean(), duration.std(), velocity().max(), velocity().min(), velocity().mean(), velocity().std(), note_highest_velocity(), density.mean(), density.std(), silence_proportion(), silence.mean(), silence.std(), *time_signature]
+    return [bpm, pitch().max(), pitch().min(), pitch().mean(), pitch().std(), proportion_high(), proportion_medium(), proportion_bass(), duration.max(), duration.min(), duration.mean(), duration.std(), velocity().max(), velocity().min(), velocity().mean(), velocity().std(), note_highest_velocity(), density.mean(), density.std(), silence_proportion(), silence.mean(), silence.std(), *time_signature, *key_signature]
 
 
 output = read_header(HEADER_FILE)
