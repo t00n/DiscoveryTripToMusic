6ecb5c85338579257238a1a7bdadee7647d8b701
t00n <kelkununtel@hotmail.com>
2016-04-19 16:05:41 +0200

    [add] parses pitch max, min, mean, std

--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,29 @@
-from pandas import read_csv
+from pandas import DataFrame, read_csv
+import csv
 
-data_repertory = "../data/"
-header_file = data_repertory + "dataset-balanced.csv"
-song_repertory = data_repertory + "songs/"
+DATA_REP = "../data/"
+HEADER_FILE = DATA_REP + "dataset-balanced.csv"
+SONG_REP = DATA_REP + "songs/"
+
+def read_song(id):
+    song_file = SONG_REP + str(id) + ".csv"
+    with open(song_file) as csvfile:
+        reader = csv.reader(csvfile)
+        return [list(map(lambda x: x.strip(), row)) for row in reader]
+
+def read_header(f):
+    return read_csv(HEADER_FILE, header=0, sep=';')
+
+def create_song_features(data):
+    notes = DataFrame(list(map(lambda x: [int(x[0]), int(x[1]), x[2], int(x[3]), int(x[4]), int(x[5])], filter(lambda x: x[2][:4] == "Note", data))))
+    def pitch():
+        return notes[4]
 
 
-output = read_csv(header_file, header=0, sep=';')
+output = read_header(HEADER_FILE)
 songs = []
 
 for index, row in output.iterrows():
-    song_file = song_repertory + str(row['id']) + ".csv"
-    # parse song file here
+    data = read_song(row['id'])
+    create_song_features(data)
+
