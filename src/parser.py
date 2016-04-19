6199fe5f94a0794398c096ee8f78515037acedfb
t00n <kelkununtel@hotmail.com>
2016-04-19 16:25:56 +0200

    [add] proportion of bass, medium, high

--- src/parser.py
+++ src/parser.py
@@ -18,6 +18,13 @@ def create_song_features(data):
     notes = DataFrame(list(map(lambda x: [int(x[0]), int(x[1]), x[2], int(x[3]), int(x[4]), int(x[5])], filter(lambda x: x[2][:4] == "Note", data))))
     def pitch():
         return notes[4]
+    def proportion_high():
+        return len(pitch()[pitch() >= 72]) / len(pitch())
+    def proportion_medium():
+        return len(pitch()[54 <= pitch()][pitch() < 72]) / len(pitch())
+    def proportion_bass():
+        return len(pitch()[pitch() < 54]) / len(pitch())
+    return [pitch().max(), pitch().min(), pitch().mean(), pitch().std(), proportion_high(), proportion_medium(), proportion_bass()]
 
 
 output = read_header(HEADER_FILE)
@@ -25,5 +32,6 @@ songs = []
 
 for index, row in output.iterrows():
     data = read_song(row['id'])
-    create_song_features(data)
+    features = create_song_features(data)
+    print(features)
 
