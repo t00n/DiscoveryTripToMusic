f19147f654749e048e7935e61c60bf67b5ab7d68
t00n <kelkununtel@hotmail.com>
2016-08-22 20:46:44 +0200

    [fix] teacher script working

--- src/prediction.py
+++ src/prediction.py
@@ -23,11 +23,10 @@ def predict_lin(training_set, output, test_set):
     return lin.predict(test_set)
 
 def prediction(training_file, test_file, features_on, output_file=''):
-    clf = OneVsOneClassifier(SVC(kernel='linear'))
-    lin = LinearRegression()
-
-    training_set, output, test_file = get_all(training_file, test_file, features_on)
-    results = [(lambda f: f(training_set, output[x], test_set))(predict_clf if TARGETS[x] == 'cls' else predict_lin) for x in TARGETS_NAMES]
+    results = []
+    for x in TARGETS_NAMES:
+        training_set, output, test_set = get_all(training_file, test_file, features_on[x])
+        results.append((lambda f: f(training_set, output[x], test_set))(predict_clf if TARGETS[x] == 'cls' else predict_lin)) 
 
     if output_file != "":
         write_prediction_csv(output_file, *results)
